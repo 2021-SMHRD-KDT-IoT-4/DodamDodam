791466fb0d17bb97cbc1c9d0d3e6cc29f2f5e73c
SMHRD <SMHRD@DESKTOP-G425NQ5>
2021-06-19 11:24:55 +0900

    상지가 푸쉬

---
+++ SecondProject/src/main/java/COM/Controller/LoginService.java
@@ -0,0 +1,31 @@
+package COM.Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginService")
+public class LoginService extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("EUC-KR");
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String name = request.getParameter("name");
+		String tel = request.getParameter("tel");
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	}
+
+}
